fa6513393172becc43c4d3d70167c36bad43b552
ravin <ravin@RAVINDRA>
2025-03-07 17:01:41 +0530

    Model Added

---
+++ NewWebApp/Models/Student.cs
@@ -0,0 +1,14 @@
+﻿namespace NewWebApp.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string name { get; set; }
+        public string Address { get; set; }
+        public string Phoneno { get; set; }
+        public string OrderDetails { get; set; }
+        public string OrderNo { get; set; }
+
+
+    }
+}
